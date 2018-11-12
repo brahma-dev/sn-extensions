3e1cd362d47b151c008db281c7824d922e5b6977
Brahma Dev <me@brahma.world>
2018-11-12 23:17:05 +0530

    Init

---
+++ README.md
@@ -0,0 +1,27 @@
+# StandardNotes Extensions
+
+Run this code in your `standardnotes/web` repo
+
+    git submodule add https://github.com/brahma-dev/sn-extensions public/extensions/sn-extensions
+
+Then adapt the following URLs to install
+
+- Advanced Markdown : [[Your Public URL]]/extensions/sn-extensions/dist/advanced-markdown-editor.json
+- Simple Markdown : [[Your Public URL]]/extensions/sn-extensions/dist/simple-markdown-editor.json
+- Plus Editor : [[Your Public URL]]/extensions/sn-extensions/dist/plus-editor.json
+- Fancy Markdown Editor : [[Your Public URL]]/extensions/sn-extensions/dist/fancy-markdown-editor.json
+- Minimal Markdown : [[Your Public URL]]/extensions/sn-extensions/dist/minimal-markdown-editor.json
+- Code Editor : [[Your Public URL]]/extensions/sn-extensions/dist/code-editor.json
+- Vim Editor : [[Your Public URL]]/extensions/sn-extensions/dist/vim-editor.json
+- Simple Task Editor : [[Your Public URL]]/extensions/sn-extensions/dist/simple-task-editor.json
+- Quick Tags : [[Your Public URL]]/extensions/sn-extensions/dist/autocomplete-tags.json
+- Action Bar : [[Your Public URL]]/extensions/sn-extensions/dist/action-bar.json
+- GitHub Push : [[Your Public URL]]/extensions/sn-extensions/dist/github-push.json
+- Folders : [[Your Public URL]]/extensions/sn-extensions/dist/folders-component.json
+- Midnight Theme : [[Your Public URL]]/extensions/sn-extensions/dist/midnight-theme.json
+- Focused Theme : [[Your Public URL]]/extensions/sn-extensions/dist/focus-theme.json
+- Futura Theme : [[Your Public URL]]/extensions/sn-extensions/dist/futura-theme.json
+- Titanium Theme : [[Your Public URL]]/extensions/sn-extensions/dist/titanium-theme.json
+- Solarized Dark Theme : [[Your Public URL]]/extensions/sn-extensions/dist/solarized-dark-theme.json
+- Dynamic Theme : [[Your Public URL]]/extensions/sn-extensions/dist/dynamic-theme.json
+- No Distraction Theme : [[Your Public URL]]/extensions/sn-extensions/dist/no-distraction-theme.json
---
+++ index.js
@@ -0,0 +1,94 @@
+const path = require("path");
+const http = require("https");
+const map = require(path.join(__dirname, './map.js'));
+const fs = require('fs');
+
+const getJSON = function(hostname, pth, opt) {
+	return new Promise(function(respond, reject) {
+		var options = {
+			"method": "GET",
+			"hostname": hostname,
+			"path": pth,
+			"headers": {
+				"Cache-Control": "no-cache",
+				"User-Agent": "nodejs"
+			}
+		};
+
+		var req = http.request(options, function(res) {
+			var chunks = [];
+
+			res.on("data", function(chunk) {
+				chunks.push(chunk);
+			});
+
+			res.on("end", function() {
+				var body = Buffer.concat(chunks);
+				respond([JSON.parse(body.toString()), opt]);
+			});
+		});
+
+		req.on('error', reject);
+
+		req.end();
+	});
+};
+
+const mapRepos = function([repos]) {
+	return new Promise(function(respond, reject) {
+		let releases = [];
+		for (let x in repos) {
+			if (map[repos[x].name]) {
+				releases.push(getJSON("api.github.com", `/repos/sn-extensions/${repos[x].name}/releases`, repos[x].name));
+			} else {
+				console.log(`Missing ${repos[x].name}`);
+			}
+		}
+		Promise.all(releases).then(function(tags) {
+			for (let x in tags) {
+				tags[x][0] = tags[x][0].filter((e) => e.draft == false && e.prerelease == false);
+				if (tags[x][0][0]) {
+					map[tags[x][1]].download_url = `https://api.github.com/repos/sn-extensions/${tags[x][1]}/zipball/${tags[x][0][0].tag_name}`;
+					map[tags[x][1]].version = tags[x][0][0].tag_name;
+					if (map[tags[x][1]].area == "themes") {
+						map[tags[x][1]].url = `https://cdn.staticaly.com/gh/sn-extensions/${tags[x][1]}/${tags[x][0][0].tag_name}/dist/dist.css`;
+						map[tags[x][1]].latest_url = `https://cdn.staticaly.com/gh/sn-extensions/${tags[x][1]}/${tags[x][0][0].tag_name}/dist/dist.css`;
+					} else {
+						map[tags[x][1]].url = `https://cdn.staticaly.com/gh/sn-extensions/${tags[x][1]}/${tags[x][0][0].tag_name}/index.html`;
+						map[tags[x][1]].latest_url = `https://cdn.staticaly.com/gh/sn-extensions/${tags[x][1]}/${tags[x][0][0].tag_name}/index.html`;
+					}
+				}
+			}
+		}).then(respond);
+	});
+};
+
+const writeJSON = function() {
+	Object.keys(map).map(function(objectKey, index) {
+		let value = map[objectKey];
+		console.log(value);
+		fs.writeFileSync(path.join(__dirname, './dist/', objectKey + ".json"), JSON.stringify(value, "", "\t") + "\n", "utf8");
+	});
+};
+
+const writeREADME = function() {
+	let md = [
+		"# StandardNotes Extensions",
+		"",
+		"Run this code in your `standardnotes/web` repo",
+		"",
+		"    git submodule add https://github.com/brahma-dev/sn-extensions public/extensions/sn-extensions",
+		"",
+		"Then adapt the following URLs to install",
+		""
+	];
+	Object.keys(map).map(function(objectKey, index) {
+		let value = map[objectKey];
+		let u = `[[Your Public URL]]/extensions/sn-extensions/dist/${objectKey}.json`;
+		md.push(`- ${value.name} : ${u}`);
+	});
+	fs.writeFileSync(path.join(__dirname, "./README.md"), md.join("\n") + "\n", "utf8");
+
+};
+
+getJSON("api.github.com", "/users/sn-extensions/repos").then(mapRepos).then(writeJSON).then(writeREADME);
